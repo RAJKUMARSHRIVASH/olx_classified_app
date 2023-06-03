ca57147d36cb0df10e2ae47ab8b44a6b9b91c363
RAJ KUMAR SEN <rajkumarshrivash374@gmail.com>
2023-06-03 15:38:26 +0530

    deployment

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,8 @@
 const express = require("express");
-const connection  = require("./config/db");
+const connection = require("./config/db");
 const userRouter = require("./routes/userRoute");
 const path = require("path");
-const app  = express();
+const app = express();
 const cors = require("cors");
 const postAdsRoute = require("./routes/postAdsRoute");
 const authentication = require("./middleware/authentication");
@@ -10,21 +10,21 @@ require("dotenv").config();
 app.use(express.json());
 app.use(cors());
 
-app.get("/",(req,res)=>{
-    express.static(__dirname,"../","frontend");
-    res.sendFile(path.join(path.resolve(__dirname,"../","frontend","index.html")));
-})
-app.use("/api/user",userRouter);
+app.get("/", (req, res) => {
+    app.use(express.static(path.join(__dirname, "../", "frontend")));
+    res.sendFile(path.resolve(__dirname, "../", "frontend", "index.html"));
+});
+app.use("/api/user", userRouter);
 
-app.use("/api/postads",authentication,postAdsRoute);
-app.use("/api/browseads",postAdsRoute);
+app.use("/api/postads", authentication, postAdsRoute);
+app.use("/api/browseads", postAdsRoute);
 
 
-app.listen(process.env.port,async()=>{
+app.listen(process.env.port, async () => {
     try {
         await connection;
         console.log(`Server is running at the port ${process.env.port}`);
     } catch (error) {
-        console.log("Something went wrong "+error);
+        console.log("Something went wrong " + error);
     }
 })
